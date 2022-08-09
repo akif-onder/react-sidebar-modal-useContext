af927e0d918b0da57320600166a8789d815d7ec3
akif-onder <akifonder@gmail.com>
2022-08-09 11:00:13 +0300

    started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Modal from './Modal'
+import Sidebar from './Sidebar'
+import Home from './Home'
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
