7bc0fc315c7b5792bc3b75180ed3cb918e2c1f2d
akif-onder <akifonder@gmail.com>
2022-08-12 12:05:40 +0300

    sidebar open close feature added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import { social, links } from './data';
+import { useGlobalContext } from './context';
+
 
 const Sidebar = () => {
+
+  const { closeSidebar, isSidebarOpen} = useGlobalContext();
+
   return (
-    <aside className={'sidebar show-sidebar'}>
+    <aside className={`${isSidebarOpen ?'sidebar show-sidebar' : 'sidebar'}`}>
       <div className='sidebar-header'>
         <img src={logo} className='logo' alt='coding addict' />
-        <button className='close-btn'>
+        <button className='close-btn' onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
       <ul className='links'>
         {links.map((link) => {
-          const { id, url, text, icon } = link;
+          const { id, url, text, icon } = link; 
           return (
             <li key={id}>
               <a href={url}>
