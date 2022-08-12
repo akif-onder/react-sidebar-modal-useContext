2d916404895aef0983180210fc084bc1d7ad56cc
akif-onder <akifonder@gmail.com>
2022-08-12 11:58:43 +0300

    context formed

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa';
+import { AppContext, useGlobalContext} from './context';
+
 
 const Home = () => {
+  const {openSidebar, openModal} = useGlobalContext();
+  
   return (
-      <main>
-        <button className='sidebar-toggle'>
-          <FaBars/>
-        </button>
-        <button className='btn'>Show Modal</button>
-        
-
-      </main>
+    <main>
+      <button className='sidebar-toggle' onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className='btn' onClick={openModal}>Show Modal</button>
+    </main>
   )
 }
 
